06ec4d7530fc870ede4c8a396fec6a30ecbfd315
Roman <romanapilov97@gmail.com>
2021-11-05 20:21:04 +0300

    first commit

---
+++ homework_2.js
@@ -0,0 +1,17 @@
+true// "кот" > "код"
+24 // ● "2" + 2 * "2"
+true// ● undefined == null
+false// ● undefined === null
+false// ● null == 0
+false // ● 2 > "3"
+1// ● null - false + true
+1// ● 1 / "l"
+6// ● "2" * "3"
+"9O"// ● 4 + 5 + "O"
+145// ● "l" + 4 + 5
+2// ● "4" - 2
+NaN// ● "4" - "4x"
+true// ● null == false
+-Infinity// ● " -4 "/ 0 + 1
+1// ● null + 1
+NaN// ● undefined + null
